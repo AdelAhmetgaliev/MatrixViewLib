ccbad2a1d00c36a098c2f28562a321c0ef9602f2
Adel Ahmetgaliev <adel.ahmetgaliev@yandex.ru>
2026-03-30 09:12:50 +0300

    Fixed the documentation

--- include/matrixview.h
+++ include/matrixview.h
@@ -10,14 +10,14 @@
 #include <stdlib.h>
 
 typedef struct _MatrixView {
-    double *dataView;   /* Поинтер на линейный массив. */
+    double *dataView;   /* Указатель на линейный массив. */
     size_t rowCount;    /* Кол-во строк в представлении. */
     size_t colCount;    /* Кол-во столбцов в представлении. */
 } MatrixView;
 
 /**
  * Создает новый экземпляр структуры MatrixView.
- * @param *dataView поинтер на линейный массив.
+ * @param *dataView указатель на линейный массив.
  * @param rowCount кол-во строк в представлении.
  * @param colCount кол-во столбцов в представлении.
  */
@@ -25,81 +25,81 @@ MatrixView mv_new(double *dataView, size_t rowCount, size_t colCount);
 
 /**
  * Выводит экземпляр структуры MatrixView в stdout.
- * @param *matrix поинтер на экземпляр структуры.
+ * @param *matrix указатель на экземпляр структуры.
  */
 void mv_print(const MatrixView *matrix);
 
 /**
  * Копирует элементы одной MatrixView в другую.
  * Размеры (число строк и число столбцов) должны быть одинаковы.
- * @param *dest поинтер на MatrixView куда идет копирование.
- * @param *src поинтер на MatrixView откуда идет копирование.
+ * @param *dest указатель на MatrixView куда идет копирование.
+ * @param *src указатель на MatrixView откуда идет копирование.
  */
 void mv_copy(MatrixView *dest, const MatrixView *src);
 
 /**
  * Копирует элементы одной MatrixView в другую.
  * Общие размеры (размеры линейных массивов) должны быть одинаковы.
- * @param *dest поинтер на MatrixView куда идет копирование.
- * @param *src поинтер на MatrixView откуда идет копирование.
+ * @param *dest указатель на MatrixView куда идет копирование.
+ * @param *src указатель на MatrixView откуда идет копирование.
  */
 void mv_copy_raw(MatrixView *dest, const MatrixView *src);
 
 /**
  * Транспонирует MatrixView.
- * @param *dest поинтер на MatrixView транспонированной матрицы.
- * @param *src поинтер на MatrixView исходной матрицы.
+ * @param *dest указатель на MatrixView транспонированной матрицы.
+ * @param *src указатель на MatrixView исходной матрицы.
  */
 void mv_transpose(MatrixView *dest, const MatrixView *src);
 
 /**
  * Суммирует элементы двух MatrixView.
- * @param *dest поинтер на MatrixView куда будут суммироваться элементы.
- * @param *left поинтер на MatrixView первого слагаемого.
- * @param *right поинтер на MatrixView второго слагаемого.
+ * @param *dest указатель на MatrixView куда будут суммироваться элементы.
+ * @param *left указатель на MatrixView первого слагаемого.
+ * @param *right указатель на MatrixView второго слагаемого.
  */
 void mv_sum(MatrixView *dest, const MatrixView *left, const MatrixView *right);
 
 /**
  * Суммирует элементы двух MatrixView. Результат помещает в первое слагаемое.
- * @param *dest поинтер на MatrixView первого слагаемого.
- * @param *src поинтер на MatrixView второго слагаемого.
+ * @param *dest указатель на MatrixView первого слагаемого.
+ * @param *src указатель на MatrixView второго слагаемого.
  */
 void mv_sumto(MatrixView *dest, const MatrixView *src);
 
 /**
  * Умножает MatrixView на double.
- * @param *matrix поинтер на MatrixView.
+ * @param *matrix указатель на MatrixView.
  * @param scalar число для умножения.
  */
 void mv_scale(MatrixView *matrix, double scalar);
 
 /**
  * Матрично умножает две MatrixView.
- * @param *dest поинтер на MatrixView куда будет записано произведение.
- * @param *left поинтер на MatrixView первого множителя.
- * @param *right поинтер на MatrixView второго множителя.
+ * @param *dest указатель на MatrixView куда будет записано произведение.
+ * @param *left указатель на MatrixView первого множителя.
+ * @param *right указатель на MatrixView второго множителя.
  */
 void mv_dot(MatrixView *dest, const MatrixView *left, const MatrixView *right);
 
 /**
  * Тензорно умножает две MatrixView.
- * @param *dest поинтер на MatrixView куда будет записано произведение.
- * @param *left поинтер на MatrixView первого множителя.
- * @param *right поинтер на MatrixView второго множителя.
+ * @param *dest указатель на MatrixView куда будет записано произведение.
+ * @param *left указатель на MatrixView первого множителя.
+ * @param *right указатель на MatrixView второго множителя.
  */
 void mv_tdot(MatrixView *dest, const MatrixView *left, const MatrixView *right);
 
 /**
  * Заполняет MatrixView случайными числами от -1.0 до 1.0.
- * @param *matrix поинтер на MatrixView.
+ * @param *matrix указатель на MatrixView.
  */
 void mv_rand(MatrixView *matrix);
 
 /**
  * Применяет функцию ко всем элементам MatrixView.
- * @param *matrix поинтер на MatrixView.
- * @param *func поинтер на функцию.
+ * @param *matrix указатель на MatrixView.
+ * @param *func указатель на функцию.
  */
 void mv_map(MatrixView *matrix, double (*func)(double));
 
